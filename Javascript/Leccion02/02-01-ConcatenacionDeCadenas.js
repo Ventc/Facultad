8d4dd68a94657ec63c0e749b4c3d007cb8969ea9
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2022-11-01 17:13:48 -0300

    Commit Clase-7-JS
    Mini modificacion clase JS

--- Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
+++ Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,22 @@ console.log(juntos);
 
 nombre += apellido; //Tercera concatenacion usando el operador simplificado
 console.log(nombre);
+
+// Hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Ludmila";
+console.log(nombre2);
+
+const apellido2 = "Rossi";
+//apellido2 = "Bossi"; una constante no puede ser modificada
+console.log(apellido2)
+
+let x, y; //Se pueden crear varias variables dentro una misma linea
+x = 17, y = 21; //Se puede hacer asignacion de varias variables dentro de la misma linea
+let z = x + y; //Se asigna el valor de la operacion
+console.log(z);
+
+let _1num = 31; //No utilizar numeros para iniciar el nombre de una variable
+let rompiendo = "rompe"; //No utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
