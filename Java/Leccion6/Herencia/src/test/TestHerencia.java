0ddbc592b6f483e95eec82fa5e6cd384ee8534ec
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2022-11-04 14:31:42 -0300

    Clase-9-Java
    Creacion de Herencia en Java

---
+++ Java/Leccion6/Herencia/src/test/TestHerencia.java
@@ -0,0 +1,18 @@
+
+package test;
+
+import domain.Cliente;
+import domain.Empleado;
+import java.util.Date;
+
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Matias", 70000.0);
+        System.out.println("empleado1 = " + empleado1);
+        
+        Date fecha1 = new Date(); 
+        
+        Cliente cliente1 = new Cliente(fecha1, true, "Ludmila", 'F', 23, "Padilla 2273");
+        System.out.println("cliente1 = " + cliente1);
+    }
+}
