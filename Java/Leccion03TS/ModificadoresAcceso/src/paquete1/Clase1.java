a84d98d75e58ef59b168bd5dcf0de2bd0eb7f5fe
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-04-27 16:38:27 -0300

    Java-Clase3-S3
    ForEach, Autoboxing, Unboxing y Modificadores de acceso.

---
+++ Java/Leccion03TS/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,23 @@
+
+package paquete1;
+
+public class Clase1 {
+    public String atributoPublic = "Valor atributo public";
+    protected String atributoProtected = "valor atributo protected";
+    
+    public Clase1(){
+        System.out.println("Constructor Public");
+    }
+    
+    protected Clase1(String atributoPublic){
+        System.out.println("Constructor Protected");
+    }
+    
+    public void metodoPublic(){
+        System.out.println("Metodo Public");
+    }
+    
+    protected void metodoProtected(){
+        System.out.println("Metodo Protected");
+    }
+}
