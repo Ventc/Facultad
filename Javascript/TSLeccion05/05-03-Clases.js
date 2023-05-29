cac64bc8173938275a3f0c1f9202e9f6eb28c9bd
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-05-29 13:26:37 -0300

    JS-Clase7-TS
    Clases parte 2 (Clase Object, toString, sobreescritura y Polimorfismo)

--- Javascript/TSLeccion05/05-03-Clases.js
+++ Javascript/TSLeccion05/05-03-Clases.js
@@ -21,6 +21,16 @@ class Persona{ //Clase padre
     set apellido(apellido){
         this._apellido = apellido;
     }
+
+    nombreCompleto(){
+        return this._nombre+" "+this._apellido;
+    }
+    //Sobreescribiendo el metodo de la clase padre (Object)
+    toString(){ //Regresa un String
+        //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecucion
+        //El metodo que se ejecuta depende si es una referencia de tipo padre o hija
+        return this.nombreCompleto();
+    }
 }
 
 class Empleado extends Persona{ //Clase hija
@@ -35,6 +45,11 @@ class Empleado extends Persona{ //Clase hija
 
     set departamento(departamento){
         this._departamento = departamento;
+    }
+
+    //Sobreescritura
+    nombreCompleto(){
+        return super.nombreCompleto()+", "+this._departamento;
     }
 }
 
@@ -57,4 +72,9 @@ console.log(persona2.apellido);
 
 let empleado1 = new Empleado("Ludmila", "Rossi", "Sistemas");
 console.log(empleado1);
-console.log(empleado1.nombre);
+console.log(empleado1.nombreCompleto());
+
+//Object.prototype.toString Esta es la manera de acceder a atributos y metodos de manera dinamica
+console.log(empleado1.toString());
+console.log(persona1.toString());
+
