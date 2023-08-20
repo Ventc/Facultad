6d3ee12469172b349e9a05ad4794e608f2fbddc8
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-08-19 22:23:57 -0300

    JS-Clase1-S4
    Proyecto E-Commerce2022

---
+++ Javascript/E-Commerce2022/client/js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 400,
+        quanty: 1,
+        img: "/client/media/banana.jpg"
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/client/media/leche.jpg"
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quanty: 1,
+        img: "/client/media/pollo.jpg"
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 250,
+        quanty: 1,
+        img: "/client/media/mayonesa natura.jpg"
+    },
+    {
+        id: 5,
+        productName: "Enlatados",
+        price: 500,
+        quanty: 4,
+        img: "/client/media/enlatados.jpg"
+    },
+    {
+        id: 6,
+        productName: "Productos Variados",
+        price: 1000,
+        quanty: 10,
+        img: "/client/media/productos variados.jpg"
+    }
+]
