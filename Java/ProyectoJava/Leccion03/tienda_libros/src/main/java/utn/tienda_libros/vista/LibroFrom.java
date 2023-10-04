c3734537dd0b835ade9d49ff7ca7121cb474cc1e
Ivan, Matias <112597945+Ventc@users.noreply.github.com>
2023-10-04 10:38:57 -0300

    Java-Clase07-S4
    Proyecto Tienda de Libros con SpringBoot y Swing -> Parte 02

---
+++ Java/ProyectoJava/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -0,0 +1,24 @@
+package utn.tienda_libros.vista;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import utn.tienda_libros.servicio.LibroServicio;
+
+import javax.swing.*;
+
+public class LibroFrom extends JFrame {
+    LibroServicio libroServicio;
+    private JPanel panel;
+
+    @Autowired
+    public LibroFrom(LibroServicio libroServicio){
+        this.libroServicio = libroServicio;
+        iniciarForma(); //Método que se encarga de tomar la informacion de la base de datos para que se visualize en el formulario a través de esta clase
+    }
+
+    private void iniciarForma(){
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900,700);
+    }
+}
